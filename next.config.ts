7dff0892671498b03356d9c1ed25c0c77e05897f
Büşra sima Yavşan <busrasimayavsan@192.168.1.9>
2026-01-10 14:17:02 +0300

    fix: External image domains eklendi (Unsplash ve carlogos.org)
    - next.config.ts'e images.remotePatterns konfigürasyonu eklendi
    - Unsplash (images.unsplash.com) domain'i eklendi - ServicesSection görselleri için
    - Carlogos.org (www.carlogos.org) domain'i eklendi - HeroSection marka logoları için
    - Hizmetlerimiz bölümündeki görseller artık production'da görünecek

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.carlogos.org',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
